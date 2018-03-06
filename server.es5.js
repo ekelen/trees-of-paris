e45c353a395527de65541aa300b32febf5ca6b5a
Elizabeth Kelen <elizabethkelen@liz.local>
2018-03-06 12:21:37 +0100

    build server

--- server.es5.js
+++ server.es5.js
@@ -1,5 +1,9 @@
 "use strict";
 
+var _promise = require("babel-runtime/core-js/promise");
+
+var _promise2 = _interopRequireDefault(_promise);
+
 var _keys = require("babel-runtime/core-js/object/keys");
 
 var _keys2 = _interopRequireDefault(_keys);
@@ -72,24 +76,32 @@ function handleError(res) {
   return res.status(code || 500).json({ error: { reason: reason, message: message, code: code } });
 }
 
-// ROUTES
-// app.get('/init', function(req, res) {
-//   addRecords()
-// })
+// MIDDLEWARE
+checkHeader = function checkHeader(req, res, next) {
+  if (!req.headers['x-auth']) return handleError(res, "You are not authorized.", null, 401);
+  if (req.headers['x-auth'] !== process.env.ADMIN_KEY) return handleError(res, "You are not authorized.", null, 401);
+  next();
+};
 
-app.get("/api/trees", function () {
+// ROUTES
+app.get('/api/init', checkHeader, function () {
   var _ref = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee(req, res) {
-    var query, options;
+    var results;
     return _regenerator2.default.wrap(function _callee$(_context) {
       while (1) {
         switch (_context.prev = _context.next) {
           case 0:
-            query = req.query;
-            options = new Options(query);
-
-            Trees.find({}, {}, options).cursor().pipe(JSONStream.stringify()).pipe(res.type('json'));
+            setTimeout(function () {
+              if (!res.headersSent) return res.send('Time out.');
+            }, 180000);
+            _context.next = 3;
+            return addNew();
 
           case 3:
+            results = _context.sent;
+            return _context.abrupt("return", res.json(results));
+
+          case 5:
           case "end":
             return _context.stop();
         }
@@ -102,12 +114,71 @@ app.get("/api/trees", function () {
   };
 }());
 
-app.get("/api/trees/search", function () {
+app.get('/api/fix_special', checkHeader, function () {
   var _ref2 = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee2(req, res) {
-    var query, dbQuery, options;
+    var results;
     return _regenerator2.default.wrap(function _callee2$(_context2) {
       while (1) {
         switch (_context2.prev = _context2.next) {
+          case 0:
+            setTimeout(function () {
+              if (!res.headersSent) return res.send('Time out.');
+            }, 180000);
+            _context2.next = 3;
+            return updateSpecial();
+
+          case 3:
+            results = _context2.sent;
+            return _context2.abrupt("return", res.json(results));
+
+          case 5:
+          case "end":
+            return _context2.stop();
+        }
+      }
+    }, _callee2, this);
+  }));
+
+  return function (_x5, _x6) {
+    return _ref2.apply(this, arguments);
+  };
+}());
+
+app.get('/api/test_private', checkHeader, function (req, res) {
+  return res.json({ message: 'You used the right header.' });
+});
+
+app.get("/api/trees", function () {
+  var _ref3 = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee3(req, res) {
+    var query, options;
+    return _regenerator2.default.wrap(function _callee3$(_context3) {
+      while (1) {
+        switch (_context3.prev = _context3.next) {
+          case 0:
+            query = req.query;
+            options = new Options(query);
+
+            Trees.find({}, {}, options).cursor().pipe(JSONStream.stringify()).pipe(res.type('json'));
+
+          case 3:
+          case "end":
+            return _context3.stop();
+        }
+      }
+    }, _callee3, this);
+  }));
+
+  return function (_x7, _x8) {
+    return _ref3.apply(this, arguments);
+  };
+}());
+
+app.get("/api/trees/search", function () {
+  var _ref4 = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee4(req, res) {
+    var query, dbQuery, options;
+    return _regenerator2.default.wrap(function _callee4$(_context4) {
+      while (1) {
+        switch (_context4.prev = _context4.next) {
           case 0:
             // const queryStr = queryString.stringify(req.query)
             query = req.query;
@@ -125,14 +196,14 @@ app.get("/api/trees/search", function () {
 
           case 5:
           case "end":
-            return _context2.stop();
+            return _context4.stop();
         }
       }
-    }, _callee2, undefined);
+    }, _callee4, undefined);
   }));
 
-  return function (_x5, _x6) {
-    return _ref2.apply(this, arguments);
+  return function (_x9, _x10) {
+    return _ref4.apply(this, arguments);
   };
 }());
 
@@ -141,7 +212,7 @@ app.use(express.static(distDir));
 
 // CONTROLLERS
 var formatGeoSearch = function formatGeoSearch(lnglat) {
-  var distance = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 200;
+  var distance = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 250;
 
   var geoQuery = {};
   var geoPt = {
@@ -189,7 +260,7 @@ function Search(query) {
           return Util.isNumberLike(v) ? Util.toNum(v) : v;
         }) };else if (Util.isNumberLike(query[k])) this[k] = Util.toNum(query[k]);
     } else if (k === "geometry") {
-      this['geometry'] = formatGeoSearch(Util.toLngLat(query[k]));
+      this['geometry'] = formatGeoSearch(Util.toLngLat(query[k]), query['distance'] ? +query['distance'] : undefined);
     }
   }
 }
@@ -202,34 +273,65 @@ function Options(query) {
   }
 }
 
-var addRecords = function () {
-  var _ref3 = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee4() {
-    return _regenerator2.default.wrap(function _callee4$(_context4) {
+var addNew = function addNew() {
+  return new _promise2.default(function (resolve) {
+    return request({ url: 'http://localhost:8080/static/data/lg/les-arbres.json' }).pipe(JSONStream.parse('*')).pipe(es.mapSync(function () {
+      var _ref5 = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee5(t) {
+        return _regenerator2.default.wrap(function _callee5$(_context5) {
+          while (1) {
+            switch (_context5.prev = _context5.next) {
+              case 0:
+                _context5.next = 2;
+                return Trees.create({
+                  id: t.recordid,
+                  species: t.fields.espece,
+                  genus: t.fields.genre,
+                  commonName: t.fields.libellefrancais,
+                  street: t.fields.adresse.toLowerCase(),
+                  arrondissement: parseInt(t.fields.arrondissement.split('').filter(function (c) {
+                    return c >= '0' && c <= '9';
+                  }).join('')),
+                  geometry: t.geometry,
+                  notable: !!+t.fields.remarquable,
+                  usage: t.fields.domanialite.toLowerCase(),
+                  circumference: parseInt(t.fields.circonferenceencm),
+                  height: parseInt(t.fields.hauteurenm)
+                }).then(function () {
+                  return console.log("ok");
+                }).catch(function (err) {});
+
+              case 2:
+              case "end":
+                return _context5.stop();
+            }
+          }
+        }, _callee5, undefined);
+      }));
+
+      return function (_x12) {
+        return _ref5.apply(this, arguments);
+      };
+    }())).on('end', function () {
+      console.log('Sort of the end.');
+      resolve('finished');
+    });
+  });
+};
+
+var updateSpecial = function () {
+  var _ref6 = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee7() {
+    return _regenerator2.default.wrap(function _callee7$(_context7) {
       while (1) {
-        switch (_context4.prev = _context4.next) {
+        switch (_context7.prev = _context7.next) {
           case 0:
             request({ url: 'http://localhost:8080/static/data/les-arbres.json' }).pipe(JSONStream.parse('*')).pipe(es.mapSync(function () {
-              var _ref4 = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee3(t) {
-                return _regenerator2.default.wrap(function _callee3$(_context3) {
+              var _ref7 = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee6(t) {
+                return _regenerator2.default.wrap(function _callee6$(_context6) {
                   while (1) {
-                    switch (_context3.prev = _context3.next) {
+                    switch (_context6.prev = _context6.next) {
                       case 0:
-                        _context3.next = 2;
-                        return Trees.create({
-                          id: t.recordid,
-                          species: t.fields.espece,
-                          genus: t.fields.genre,
-                          commonName: t.fields.libellefrancais,
-                          street: t.fields.adresse.toLowerCase(),
-                          arrondissement: parseInt(t.fields.arrondissement.split('').filter(function (c) {
-                            return c >= '0' && c <= '9';
-                          }).join('')),
-                          geometry: t.geometry,
-                          notable: !!t.fields.remarquable,
-                          usage: t.fields.domanialite.toLowerCase(),
-                          circumference: parseInt(t.fields.circonferenceencm),
-                          height: parseInt(t.fields.hauteurenm)
-                        }).then(function () {
+                        _context6.next = 2;
+                        return Trees.update({ id: t.recordid }, { $set: { notable: !!+t.fields.remarquable } }).then(function () {
                           return console.log("ok");
                         }).catch(function (err) {
                           return console.log(err.message);
@@ -237,26 +339,28 @@ var addRecords = function () {
 
                       case 2:
                       case "end":
-                        return _context3.stop();
+                        return _context6.stop();
                     }
                   }
-                }, _callee3, undefined);
+                }, _callee6, undefined);
               }));
 
-              return function (_x8) {
-                return _ref4.apply(this, arguments);
+              return function (_x13) {
+                return _ref7.apply(this, arguments);
               };
-            }()));
+            }())).then(function () {
+              return 'This will not work, need request-promise.';
+            });
 
           case 1:
           case "end":
-            return _context4.stop();
+            return _context7.stop();
         }
       }
-    }, _callee4, undefined);
+    }, _callee7, undefined);
   }));
 
-  return function addRecords() {
-    return _ref3.apply(this, arguments);
+  return function updateSpecial() {
+    return _ref6.apply(this, arguments);
   };
 }();
